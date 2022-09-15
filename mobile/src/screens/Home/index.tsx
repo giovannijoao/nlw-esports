71b36caed688ff84fcc9724e6fd9772a2369d69b
João Giovanni <joaogiovannivilela@gmail.com>
2022-09-14 21:05:45 -0300

    Header component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 
 import { styles } from './styles';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Header } from '../../components/Header';
 
 export function Home() {
   return (
     <View style={styles.container}>
-
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+      <Header
+        title='Encontre seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
+      />
     </View>
   );
 }
